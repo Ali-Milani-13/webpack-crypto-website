5bd3c851fad2d08f44329cd09601ed8bb752a91a
Ali Milani <haythamtemplar@hotmail.com>
2022-04-27 00:02:46 +0430

    part of basic structure created 	modified:   src/index.html temporary test codes added and css file imported	modified:   src/index.js part of styles added	modified:   src/styles/style.css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles/style.css";
+
+/* fetch("https://coinlib.io/api/v1/coin?key=56b27c4eeda50743&pref=EUR&symbol=BTC")
+  .then((r) => r.json())
+  .then((t) => console.log(JSON.stringify(Math.round(t.price))));
+const frm = document.querySelector(".coin-frm");
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("submitted");
+});
+*/
