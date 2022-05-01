8407d3303832b8d661b382c6abd58b33fea185ee
Ali Milani <haythamtemplar@hotmail.com>
2022-05-01 21:04:26 +0430

    [TEST] Add image section with informations:   src/index.html

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import RadioBtn from "./components/RadioButtons.html";
 const frm = document.getElementById("input-frm");
 const radio = document.getElementsByTagName("input");
 const image = document.getElementById("currency-image");
+const currencyName = document.getElementById("currency-name");
 frm.innerHTML += RadioBtn;
 const clicked = (e) => {
   console.log(e.target.value);
@@ -11,8 +12,31 @@ const clicked = (e) => {
     case "BTC":
       image.setAttribute(
         "src",
-        "https://cdn-icons-png.flaticon.com/512/1490/1490900.png"
+        "https://cdn-icons-png.flaticon.com/512/5968/5968260.png"
       );
+      currencyName.innerText = "Name : Bitcoin";
+      break;
+    case "ETH":
+      currencyName.innerText = "Name : Ethereum";
+      image.setAttribute(
+        "src",
+        "https://cdn-icons-png.flaticon.com/512/7016/7016523.png"
+      );
+      break;
+    case "LTC":
+      currencyName.innerText = "Name : Litecoin";
+      image.setAttribute(
+        "src",
+        "https://cdn-icons-png.flaticon.com/512/1418/1418092.png"
+      );
+      break;
+    case "XLM":
+      currencyName.innerText = "Name : Stellar";
+      image.setAttribute(
+        "src",
+        "https://cdn-icons-png.flaticon.com/512/6001/6001831.png"
+      );
+      break;
   }
   fetch(
     `https://coinlib.io/api/v1/coin?key=56b27c4eeda50743&pref=USD&symbol=${e.target.value}`
