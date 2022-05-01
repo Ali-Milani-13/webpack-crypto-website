b1cb4760d9af4a4c98b3f6e22165c88bdc026ff2
Ali Milani <haythamtemplar@hotmail.com>
2022-05-01 16:58:16 +0430

    [TEST] Add smooth selection effect:   src/components/RadioButtons.html

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import "./styles/style.css";
 import "./styles/input.styles.css";
 import RadioBtn from "./components/RadioButtons.html";
 const frm = document.getElementById("input-frm");
+const radio = document.getElementsByTagName("input");
+const image = document.getElementById("currency-image");
 frm.innerHTML += RadioBtn;
+const clicked = (e) => {
+  console.log(e.target.value);
+  switch (e.target.value) {
+    case "BTC":
+      image.setAttribute(
+        "src",
+        "https://cdn-icons-png.flaticon.com/512/1490/1490900.png"
+      );
+  }
+  fetch(
+    `https://coinlib.io/api/v1/coin?key=56b27c4eeda50743&pref=USD&symbol=${e.target.value}`
+  )
+    .then((r) => r.json())
+    .then((t) =>
+      console.log(JSON.stringify(parseFloat(parseFloat(t.price).toFixed(2))))
+    );
+};
 
-/* fetch("https://coinlib.io/api/v1/coin?key=56b27c4eeda50743&pref=EUR&symbol=BTC")
-  .then((r) => r.json())
-  .then((t) => console.log(JSON.stringify(Math.round(t.price))));
-const frm = document.querySelector(".coin-frm");
-frm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("submitted");
-});
-*/
+for (const element of radio) {
+  element.addEventListener("click", clicked);
+}
